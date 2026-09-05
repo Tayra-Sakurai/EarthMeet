e5f4493491683a5ae1e2dacedee4b570d29f4969
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-09-05 11:51:36 +0900

    Added license header.

--- EarthMeet.Bus/Messages/FileUploadRequestedMessage.cs
+++ EarthMeet.Bus/Messages/FileUploadRequestedMessage.cs
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2026 Tayra Sakurai
+// SPDX-License-Identifier: GPL-3.0-or-later
 using CommunityToolkit.Mvvm.Messaging.Messages;
 using System;
 using System.Collections.Generic;
